4bd55075e2197c767d5ebe96f7efb131154b4f0d
Dapo Ajala <dapo@sephia.io>
2024-03-24 14:52:14 +0100

    updated deps

--- flake.nix
+++ flake.nix
@@ -1,13 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
@@ -25,23 +20,19 @@
             pkgs = nixpkgs.legacyPackages.${system};
           in
           {
-            languages = {
-              go = {
-                enable = true;
-                version = "1.21.5";
-              };
-
-              rust = {
-                enable = true;
-                channel = "stable";
-              };
-            };
-
-
             default = devenv.lib.mkShell {
               inherit inputs pkgs;
               modules = [
                 {
+                  languages = {
+                    go = {
+                      enable = true;
+                    };
+                    rust = {
+                      enable = true;
+                    };
+                  };
+
                   packages = with pkgs; [
                     # Protobuf
                     protobuf
